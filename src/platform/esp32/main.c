8d6a26480d8b35fb51387ed0250f9e8afc5030c2
Jason Hao <jasonhao518@gmail.com>
2025-09-27 11:45:12 +0200

    fix build

--- src/platform/esp32/main.c
+++ src/platform/esp32/main.c
@@ -35,11 +35,11 @@
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
-#include "lwm2mclient.h"
-#include "pb.h"
-#include "pb_decode.h"
+#include "examples/client/lwm2mclient.h"
+#include "examples/shared/dtlsconnection.h"
+#include "examples/shared/tinydtls/dtls_debug.h"
 //#define LWM2M_SERVER_URI "coaps://192.168.10.148:5685"
-#define WITH_TINYDTLS 1
+//#define WITH_TINYDTLS 1
 #define OBJ_ARRAY_SIZE 4
 static const char *TAG = "client";
 static uint8_t rx_buffer[2048];
@@ -53,8 +53,6 @@ float tsens_out;
 char serialNumber[64] = {0};
 
 RTC_FAST_ATTR uint8_t proto_buffer[8000]; // Buffer for lwm2m proto model
-
-Lwm2mModel proto_model = Lwm2mModel_init_zero;
 
 esp_err_t flash_readSerialFromFactory(char *serial_out) {
   const esp_partition_t *serial_part = esp_partition_find_first(
@@ -388,112 +386,6 @@ static void client_task(void *pvParameters)
 }
 
 
-
-#define FUNC_SEND_WAIT_SEM(func, sem) do {\
-        esp_err_t __err_rc = (func);\
-        if (__err_rc != ESP_OK) { \
-            ESP_LOGE(LOG_TAG, "%s, message send fail, error = %d", __func__, __err_rc); \
-        } \
-        xSemaphoreTake(sem, portMAX_DELAY); \
-} while(0);
-
-#define LOG_TAG "PERIODIC_SYNC"
-#define EXT_SCAN_DURATION     0
-#define EXT_SCAN_PERIOD       0
-
-static char remote_device_name[ESP_BLE_ADV_NAME_LEN_MAX] = "ESP_EXTENDED_ADV";
-static SemaphoreHandle_t test_sem = NULL;
-
-static esp_ble_ext_scan_params_t ext_scan_params = {
-    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
-    .filter_policy = BLE_SCAN_FILTER_ALLOW_ALL,
-    .scan_duplicate = BLE_SCAN_DUPLICATE_ENABLE,
-    .cfg_mask = ESP_BLE_GAP_EXT_SCAN_CFG_UNCODE_MASK | ESP_BLE_GAP_EXT_SCAN_CFG_CODE_MASK,
-    .uncoded_cfg = {BLE_SCAN_TYPE_ACTIVE, 40, 40},
-    .coded_cfg = {BLE_SCAN_TYPE_ACTIVE, 40, 40},
-};
-
-static esp_ble_gap_periodic_adv_sync_params_t periodic_adv_sync_params = {
-    .filter_policy = 0,
-    .sid = 0,
-    .addr_type = BLE_ADDR_TYPE_RANDOM,
-    .skip = 10,
-    .sync_timeout = 1000,
-};
-
-bool periodic_sync = false;
-
-
-static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
-{
-    switch (event) {
-    case ESP_GAP_BLE_SET_EXT_SCAN_PARAMS_COMPLETE_EVT:
-        xSemaphoreGive(test_sem);
-        ESP_LOGI(LOG_TAG, "Extended scanning params set, status %d", param->set_ext_scan_params.status);
-        break;
-    case ESP_GAP_BLE_EXT_SCAN_START_COMPLETE_EVT:
-        xSemaphoreGive(test_sem);
-        ESP_LOGI(LOG_TAG, "Extended scanning start, status %d", param->ext_scan_start.status);
-        break;
-    case ESP_GAP_BLE_EXT_SCAN_STOP_COMPLETE_EVT:
-        xSemaphoreGive(test_sem);
-        ESP_LOGI(LOG_TAG, "Extended scanning stop, status %d", param->period_adv_stop.status);
-        break;
-    case ESP_GAP_BLE_PERIODIC_ADV_CREATE_SYNC_COMPLETE_EVT:
-        ESP_LOGI(LOG_TAG, "Periodic advertising create sync, status %d", param->period_adv_create_sync.status);
-        break;
-    case ESP_GAP_BLE_PERIODIC_ADV_SYNC_CANCEL_COMPLETE_EVT:
-        ESP_LOGI(LOG_TAG, "Periodic advertising sync cancel, status %d", param->period_adv_sync_cancel.status);
-        break;
-    case ESP_GAP_BLE_PERIODIC_ADV_SYNC_TERMINATE_COMPLETE_EVT:
-        ESP_LOGI(LOG_TAG, "Periodic advertising sync terminate, status %d", param->period_adv_sync_term.status);
-        break;
-    case ESP_GAP_BLE_PERIODIC_ADV_SYNC_LOST_EVT:
-        ESP_LOGI(LOG_TAG, "Periodic advertising sync lost, sync handle %d", param->periodic_adv_sync_lost.sync_handle);
-        break;
-    case ESP_GAP_BLE_PERIODIC_ADV_SYNC_ESTAB_EVT:
-        ESP_LOGI(LOG_TAG, "Periodic advertising sync establish, status %d", param->periodic_adv_sync_estab.status);
-        ESP_LOGI(LOG_TAG, "address "ESP_BD_ADDR_STR"", ESP_BD_ADDR_HEX(param->periodic_adv_sync_estab.adv_addr));
-        ESP_LOGI(LOG_TAG, "sync handle %d sid %d perioic adv interval %d adv phy %d", param->periodic_adv_sync_estab.sync_handle,
-                                                                                      param->periodic_adv_sync_estab.sid,
-                                                                                      param->periodic_adv_sync_estab.period_adv_interval,
-                                                                                      param->periodic_adv_sync_estab.adv_phy);
-        break;
-    case ESP_GAP_BLE_EXT_ADV_REPORT_EVT: {
-        uint8_t *adv_name = NULL;
-        uint8_t adv_name_len = 0;
-	    adv_name = esp_ble_resolve_adv_data_by_type(param->ext_adv_report.params.adv_data,
-                                            param->ext_adv_report.params.adv_data_len,
-                                            ESP_BLE_AD_TYPE_NAME_CMPL,
-                                            &adv_name_len);
-	    if ((adv_name != NULL) && (memcmp(adv_name, remote_device_name, adv_name_len) == 0) && !periodic_sync) {
-            // Note: If there are multiple devices with the same device name, the device may sync to an unintended one.
-            // It is recommended to change the default device name to ensure it is unique.
-            periodic_sync = true;
-	        char adv_temp_name[30] = {'0'};
-	        memcpy(adv_temp_name, adv_name, adv_name_len);
-	        ESP_LOGI(LOG_TAG, "Create sync with the peer device %s", adv_temp_name);
-            periodic_adv_sync_params.sid = param->ext_adv_report.params.sid;
-	        periodic_adv_sync_params.addr_type = param->ext_adv_report.params.addr_type;
-	        memcpy(periodic_adv_sync_params.addr, param->ext_adv_report.params.addr, sizeof(esp_bd_addr_t));
-            esp_ble_gap_periodic_adv_create_sync(&periodic_adv_sync_params);
-	    }
-    }
-        break;
-    case ESP_GAP_BLE_PERIODIC_ADV_REPORT_EVT:
-        ESP_LOGI(LOG_TAG, "Periodic adv report, sync handle %d, data status %d, data len %d, rssi %d", param->period_adv_report.params.sync_handle,
-                                                                                                    param->period_adv_report.params.data_status,
-                                                                                                    param->period_adv_report.params.data_length,
-                                                                                                    param->period_adv_report.params.rssi);
-        // print periodic adv data
-        ESP_LOG_BUFFER_HEXDUMP(LOG_TAG, param->period_adv_report.params.data, param->period_adv_report.params.data_length, ESP_LOG_INFO);
-        break;
-
-    default:
-        break;
-    }
-}
-
 void app_main33(void)
 {
         // Default config
@@ -504,18 +396,14 @@ void app_main33(void)
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(example_connect());
+    //ESP_ERROR_CHECK(example_connect());
+    dtls_set_log_level(DTLS_LOG_DEBUG);
     ESP_ERROR_CHECK(temp_sensor_read_celsius(&tsens_out));
     ESP_LOGI(TAG, "Temperature: %.2f °C", tsens_out);
 
     // if proto buffer is not empty, parse it
     if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER && proto_buffer[0] != 0) {
-        pb_istream_t stream = pb_istream_from_buffer(proto_buffer, sizeof(proto_buffer));
-        if (!pb_decode(&stream, Lwm2mModel_fields, &proto_model)) {
-            ESP_LOGE(TAG, "Failed to parse LwM2M proto model: %s", PB_GET_ERROR(&stream));
-        } else {
-            ESP_LOGI(TAG, "Parsed LwM2M proto model successfully");
-        }
+
     } else {
         ESP_LOGI(TAG, "No LwM2M proto model in buffer, starting fresh");
     }
@@ -524,45 +412,7 @@ void app_main33(void)
 
     // handle device advertisement and report
     //xTaskCreate(device_report_task, "device_advertisement", 8192, NULL, 5, NULL);
-    
-        // handle device advertisement and report
-        ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-    esp_err_t ret;
-    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-    ret = esp_bt_controller_init(&bt_cfg);
-    if (ret) {
-        ESP_LOGE(LOG_TAG, "%s initialize controller failed: %s", __func__, esp_err_to_name(ret));
-        return;
-    }
-
-    ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
-    if (ret) {
-        ESP_LOGE(LOG_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
-        return;
-    }
-
-    ret = esp_bluedroid_init();
-    if (ret) {
-        ESP_LOGE(LOG_TAG, "%s init bluetooth failed: %s", __func__, esp_err_to_name(ret));
-        return;
-    }
-    ret = esp_bluedroid_enable();
-    if (ret) {
-        ESP_LOGE(LOG_TAG, "%s enable bluetooth failed: %s", __func__, esp_err_to_name(ret));
-        return;
-    }
-    ret = esp_ble_gap_register_callback(gap_event_handler);
-    if (ret){
-        ESP_LOGE(LOG_TAG, "gap register error, error code = %x", ret);
-        return;
-    }
-
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-
-    test_sem = xSemaphoreCreateBinary();
-
-    FUNC_SEND_WAIT_SEM(esp_ble_gap_set_ext_scan_params(&ext_scan_params), test_sem);
-    FUNC_SEND_WAIT_SEM(esp_ble_gap_start_ext_scan(EXT_SCAN_DURATION, EXT_SCAN_PERIOD), test_sem);
-
+  
+ 
 
 }
